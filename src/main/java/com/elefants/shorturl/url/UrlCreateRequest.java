311c8c4590537c9ab5469a763232c8285ca28588
AndriiHubenok <andrewhu08@gmail.com>
2024-04-20 23:31:18 +0300

    Fixed and Added some features in Url logic2

--- src/main/java/com/elefants/shorturl/url/UrlCreateRequest.java
+++ src/main/java/com/elefants/shorturl/url/UrlCreateRequest.java
@@ -5,13 +5,5 @@ import lombok.RequiredArgsConstructor;
 
 @Data
 public class UrlCreateRequest {
-    //private String shortUrl;
     private String longUrl;
-    //private Long score;
-    //private Boolean isActive;
-    //private String userId;
-
-    /*public UrlCreateRequest(String longUrl) {
-        this.longUrl = longUrl;
-    }*/
 }
