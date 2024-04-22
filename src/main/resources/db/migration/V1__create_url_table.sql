502337f5ff495ecb3ace70c07588b665c2ac5eed
Zahar Fedorchuk <puzeblin888@gmail.com>
2024-04-22 17:16:01 +0300

    fix properties

--- src/main/resources/db/migration/V1__create_url_table.sql
+++ src/main/resources/db/migration/V1__create_url_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE  IF NOT EXISTS users (
         username VARCHAR(50) PRIMARY KEY,
-        password VARCHAR NOT NULL,
+        password TEXT NOT NULL,
         role VARCHAR NOT NULL
 );
 
